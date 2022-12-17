38356be9d7545bcecd6c382c9e172cf1ce2d9c16
pangshanshan <pangshanshan0113@163.com>
2022-12-17 23:58:47 +0800

    js学习：js语法基础

---
+++ js-learn.js
@@ -0,0 +1,70 @@
+var name ="shiyanlou";
+var age = 18
+var isnewyear = true
+var array = [1, 2, 3, "nihao", true]
+var person = {name: 'shanshan', age: 18}
+if (isnewyear) {
+    age = age +1
+
+}
+
+var appleNums = 3
+var person1 ={name: 'zhangsan', hasAppleNums: 0}
+
+for (var i= 0; i< appleNums; i++) {
+    person1.hasAppleNums = person1.hasAppleNums + 1
+}
+
+
+
+x = 50;// 变量x为整数
+var x1 = 10;
+var x2 = 3.1415926;
+var carname = "shiyanlou";
+var carname = "shiyanlou";
+var answer = "I Love 'shiyanlou'";
+var answer = 'I love "shiyanlou"';
+var myNameArray = ["Tom","Bob","jim"];
+var myNumbeerArray = [10,15,20];
+var student = { name: "Tom",age: 18 };
+var string = "Hello";
+var mystring = "500" ;typeof mystring
+"string"
+var num = 500;typeof num
+"number"
+var p =true; typeof p
+"boolean"
+var student = { name : 'Tom',age: 18};typeof student
+"object"
+// 这是一个变量
+/*var name = "zhangshan";
+* var age = 18;
+* console.log(name,age);
+*/
+var num =3;num++
+var num =3;++num
+var num =3;num--
+var num =3;--num
+var num1 =3;
+var result1 =num1++;
+console.log(result1);
+console.log(num1);
+var num2 =4;
+var result2 =++num2;
+console.log(result2);
+console.log(num2);
+var myarray =new Array(1,2,3,4,5);
+var myarray =[1,2,3,4,5];
+var car = {name: 'dazh0ng', price: 10000}
+var person = {name: 'zhangsan', sex: 'w', car: car}
+var color =["red","green","blue","yellow"];
+color[0]//returns "red"
+var student =[
+    ["zhangshan","nan","18"]
+        ["lisi","nv","26"]
+
+]
+
+
+
+
